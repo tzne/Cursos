c07cf38f80daa668fde3a0f256d6e1c44650e0f7
Felipe Canteiro <fceolsitpae@gmail.com>
2024-03-08 05:20:07 -0300

    Início do Curso 'Linguagem C'

---
+++ Linguagem C - Estudonauta/Exercícios/ex001.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main() {
+  printf("Listagem de alunos\n");
+  printf("Nome\t Nota\n");
+  printf("--------------------\n");
+  printf("Ana Beatriz\t 8.5\n");
+  printf("Bianca Martins\t 9.0\n");
+  printf("Cláudio Sá\t 5.5\n");
+  printf("Giovana Silva\t 7.5\n");
+}
---
+++ Linguagem C - Estudonauta/Exercícios/ex002.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main() {
+  printf("==========================================\n");
+  printf("\\a \t = \t Beep \n");
+  printf("\\n \t = \t Nova Linha \n");
+  printf("\\t \t = \t Tabulação \n");
+  printf("\\\\ \t = \t Contrabarra \n");
+  printf("%%%% \t = \t Porcentagem \n");
+  printf("\\? \t = \t Interrogação \n");
+  printf("==========================================\n");
+  
+}
