d74b6e07bd182fe7d9a50b77d5ec764e61c50429
Felipe Costa Canteiro <47993341+tzne@users.noreply.github.com>
2025-05-10 18:47:51 -0300

    Java

---
+++ Java para Iniciantes/Exercícios/Ex001.java
@@ -0,0 +1,19 @@
+package ex001;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.util.Date;
+
+public class Ex001 {
+
+    public static void main(String[] args) {
+        String data = new Date().toString();
+        System.out.println("Hora do sistema: ".concat(data));
+        
+        String lingua = System.getProperty("user.language");
+        System.out.println("Idioma do sitema: ".concat(lingua));
+        
+        Dimension resolucao = Toolkit.getDefaultToolkit().getScreenSize();
+        System.out.println("Resolução do sistema: " + resolucao.getWidth() + "x" + resolucao.getHeight());
+    }
+}
---
+++ Java para Iniciantes/Exercícios/Ex002.java
@@ -0,0 +1,20 @@
+package ex002;
+
+import java.util.Scanner;
+
+public class Ex002 {
+  public static void main(String[] args) {
+    Scanner entradaPadrao = new Scanner(System.in);
+
+    System.out.print("Insira o primeiro número: ");
+    float numero1 = entradaPadrao.nextFloat();
+    System.out.print("Insira o segundo número: ");
+    float numero2 = entradaPadrao.nextFloat();
+
+    float soma = numero1 + numero2;
+
+    System.out.printf("%.2f + %.2f = %.2f", numero1, numero2, soma);
+
+
+  }
+}
